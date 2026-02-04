dbba6c235191ae14f5e1cb89cd91edffe6d588c7
David Show <david.show5c@gmail.com>
2026-02-04 07:26:16 -0600

    Initial Commit

---
+++ swipeAccountsShow/swipeAccountsShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  swipeAccountsShowApp.swift
+//  swipeAccountsShow
+//
+//  Created by DAVID SHOW on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct swipeAccountsShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
