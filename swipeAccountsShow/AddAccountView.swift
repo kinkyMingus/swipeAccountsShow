35214ec412e24f76514c6f88820f1332c6fec770
David Show <david.show5c@gmail.com>
2026-02-06 08:09:56 -0600

    Add those Accounts

---
+++ swipeAccountsShow/AddAccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AddAccountView.swift
+//  swipeAccountsShow
+//
+//  Created by DAVID SHOW on 2/6/26.
+//
+
+import SwiftUI
+
+struct AddAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddAccountView()
+}
